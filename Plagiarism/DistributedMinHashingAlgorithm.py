d19424b19445d2ac2badc8b0b8b7d47ccadcef38
CedricMingneau <cedric.mingneau@gmail.com>
2019-10-24 16:38:15 +0200

    Create DistributedMinHashingAlgorithm.py

---
+++ Plagiarism/DistributedMinHashingAlgorithm.py
@@ -0,0 +1,8 @@
+for h in p_hashes:  # p_hashes is the collection of permutation hash functions
+    # shingle_hash is the hash function that hashed the shingles image(shingle_hash) == all row indices of bit vector!
+    for x in image(shingle_hash):
+        # hashed_shingle_multiset contains all hashed shingle of a document
+        if (h(x) in hashed_shingle_multiset):
+            file_signature_matrix.add(x)
+            break
+# the image of every p_hash function must equal the image of shingle_hash function
