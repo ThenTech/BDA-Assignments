bfca06f921625bff6691cb3ec4aa4fc8e1b2477c
Wosser1sProductions <wosser1@hotmail.com>
2019-11-05 18:02:44 +0100

    Update docs

--- Plagiarism/Resources/DistributedMinHashingAlgorithm.py
+++ Plagiarism/Resources/DistributedMinHashingAlgorithm.py
@@ -1,11 +1,27 @@
+
+# For each value from the shingles hash domain
+for x in image(shingle_hash):
+    # For each col/file in SignMatrix
+    for col in range(len(SignMatrix[0])):
+        # Iterate over permutation hashes (rows from SM)
+        for row, h in enumerate(pHashes):
+            hashval = h(x)
+            # Check if the new value is lower, and replace
+            if hashval < SignMatrix[row][col]:
+                SignMatrix[row][col] = hashval
+
+
+
+# ------------------------------------------------------------------------------
+
 # p_hashes is the collection of permutation hash functions
 for h in p_hashes:
-    # shingle_hash is the hash function that hashed the 
+    # shingle_hash is the hash function that hashed the
     # shingles image(shingle_hash) == all row indices of bit vector!
     for x in image(shingle_hash):
         # hashed_shingle_multiset contains all hashed shingle of a document
         if (h(x) in hashed_shingle_multiset):
             file_signature_matrix.add(x)
             break
-# the image of every p_hash function must equal 
+# the image of every p_hash function must equal
 # the image of shingle_hash function
